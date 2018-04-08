b1c0ff7d55e5449246720e1529dcdd4c75948d1a
Gharbi Ayoub <ayoub97ma@gmail.com>
2018-04-08 01:10:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ayoub/firstapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.ayoub.firstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView_progress.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY +
+                        progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                textView_progress.animate().translationY(translationDistance)
+                if (!fromUser)
+                    textView_progress.animate().setDuration(500).rotationBy(360f)
+                            .translationY(initialTextViewTranslationY)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        button_reset.setOnClickListener { v ->
+            seekBar.progress = 0
+        }
+    }
+}
